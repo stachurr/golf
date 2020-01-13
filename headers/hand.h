825fc53e7d1b1fe0c537f64c0f3085bd2613849d
Ryan Stachura <stachurr@oregonstate.edu>
2020-01-13 22:08:19 +0000

    added the ability to choose the number of players (2-4). Also added an owner string to the Hand class

--- headers/hand.h
+++ headers/hand.h
@@ -8,14 +8,17 @@ class Hand {
 	private:
 		Card hand[6];
 		Card drawn;
+		string owner;
 
 	public:
 		Hand() {}
 		~Hand() {}
 		Card get_drawn();
 		Card get_card(int);
+		string get_owner();
 		void set_drawn(Card);
 		void set_card(Card, int);
+		void set_owner(string);
 };
 
 //get drawn card
@@ -36,4 +39,14 @@ void Hand::set_drawn(Card c) {
 //set card in hand
 void Hand::set_card(Card c, int idx) {
 	hand[idx] = c;
+}
+
+//set name
+void Hand::set_owner(string o) {
+	owner = o;
+}
+
+//get name
+string Hand::get_owner() {
+	return owner;
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 //headers
@@ -10,17 +11,47 @@ using namespace std;
 
 //prototypes
 void deal(Hand&, Pile&);
+void print_hand(Hand&);
 
 int main() {
 
 	//create objects
-	Draw_Pile	draw_pile;
-	Pile			discard_pile;
-	Hand			hand1, hand2;
+	Draw_Pile	drp;
+	Pile		dcp;
+	Hand		h1, h2, h3, h4;
+
+	int players;
+	vector<Hand> hands_in_use;
+
+	//choose number of players (2-4)
+	while (players < 2 || players > 4) {
+		cout << "How many players? (2-4): ";
+		cin >> players;
+	}
 
 	//deal hands
-	deal(hand1, draw_pile);
-	deal(hand2, draw_pile);
+	switch (players) {
+		case 4:
+			hands_in_use.push_back(h4);
+			deal(h4, drp);
+		case 3:
+			hands_in_use.push_back(h3);
+			deal(h3, drp);
+		case 2:
+			hands_in_use.push_back(h2);
+			deal(h2, drp);
+		default:
+			hands_in_use.push_back(h1);
+			deal(h1, drp);
+	}
+
+	//set names
+	string name;
+	for (int i = 0; i < hands_in_use.size(); i++) {
+		cout << "Player " << i+1 << "'s name: ";
+		cin >> name;
+		hands_in_use.at(i).set_owner(name);
+	}
 
 	return 0;
 }
@@ -32,4 +63,15 @@ void deal(Hand &h, Pile &dp) {
 		dp.erase_top();
 		h.set_card(top, i);
 	}
+}
+
+//print hand
+void print_hand(Hand &h) {
+	cout << h.get_card(0).get_value() << " of " << h.get_card(0).get_suit() << endl;
+	cout << h.get_card(1).get_value() << " of " << h.get_card(1).get_suit() << endl;
+	cout << h.get_card(2).get_value() << " of " << h.get_card(2).get_suit() << endl;
+	cout << h.get_card(3).get_value() << " of " << h.get_card(3).get_suit() << endl;
+	cout << h.get_card(4).get_value() << " of " << h.get_card(4).get_suit() << endl;
+	cout << h.get_card(5).get_value() << " of " << h.get_card(5).get_suit() << endl;
+	cout << endl;
 }
